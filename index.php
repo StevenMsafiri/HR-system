165e6939ce9f3ce2a104e4d5bac61168de2e942f
StevenMsafiri <ianleonardmk47@gmail.com>
2025-02-21 16:54:15 +0300

    Login

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
-<?php
-require_once './db_config.php';
+<!DOCTYPE html>
+<html lang="en">
 
-// header("Content-Type: application/json");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/header.css">
+    <title>Document</title>
+</head>
 
-if ($_SERVER['REQUEST_METHOD']=='GET'){
+<body>
+    <div class="nav-menu">
+        <div class="navbar" id="nav-menu">
+            <a href="#home">Home</a>
+            <a href="#news">Reports</a>
+            <div class="dropdown">
+                <button class="dropbtn">Staffs
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Departments</a>
+                    <a href="#">Sections</a>
+                    <a href="#">Employees</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
 
-    // require_once '../db_config.php';
-
-    // header("Content-Type: application/json");
-    
-    // if (isset($_GET['section'])) {
-    //     $section = $_GET['section'];
-    
-        try {
-            $stmt = $conn->prepare("SELECT last_name FROM Employees WHERE section = ? AND position = ?");
-            $stmt->execute(['IT', 'Supervisor']);
-            $supervisors = $stmt->fetchAll(PDO::FETCH_COLUMN);
-            echo $supervisors;
-            print_r($supervisors);
-
-            echo $supervisors[0];
-        } catch (PDOException $e) {
-            echo $e;
-        }
-    }
-// }
-?>
+</html>
---
+++ login/index.php
@@ -0,0 +1,79 @@
+<?php
+
+require "../db_config.php";
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(!empty($username) && !empty($password)){
+
+        try{
+            $query = "SELECT * FROM logins WHERE username = ? AND password =?";
+            $stmt = $conn->prepare($query);
+            $stmt->execute([$username,$password]);
+            $details = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if($details){
+
+                header("Location: ../index.php");
+                exit();
+
+
+            }else{
+                echo "No details found";
+                exit();
+            }
+
+            $conn = null;
+            $stmt = null;
+
+        }catch(PDOException $e){
+            echo "Failed to access employee: ". $e->getMessage();
+        }
+    }else{
+        
+        header("Location: /login/index.php");
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login Page</title>
+</head>
+<body>
+
+<div class="container-reg">
+    <div class="title-container">
+        <div class="title">
+        <h2 id="title">PLEASE LOG ON</h2>
+        </div>
+    </div>
+
+    <form  class="login-form" method="POST">
+        <div class="input-box">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="input" >
+        </div>
+        <div class="input-box">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="input">
+        </div>
+
+        <div class="right">
+        <button type="submit" name="submit" id="login-btn">Log on</button>
+        </div>
+
+    </form>
+</div>
+    
+</body>
+</html>
